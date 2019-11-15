14f4745720922fb03436853ed8bfd0cdd27c3a97
Serik.Uvaissov <Uvaissov@gmail.com>
2019-11-15 17:11:30 +0600

    MapShow.js

--- src/pages/boutique/view/MapShow.js
+++ src/pages/boutique/view/MapShow.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import { View, StyleSheet, Modal } from 'react-native'
+import { View, StyleSheet, Modal, Text, Button } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import FastImage from 'react-native-fast-image'
 import PhotoView from 'react-native-photo-view'
 import { BlockTitleAndButton } from '../../../components/ui/kit/BlockTitleAndButton'
-import { w, BLACK } from '../../../constants/global'
+import { w, BLACK, WHITE } from '../../../constants/global'
 
 const bannerW = w - 30
 const bannerH = bannerW * 0.5
@@ -32,16 +32,24 @@ const MapShow = ({ data, onLayourRef }) => {
           <FastImage source={require('../../../../resources/image/map.png')} style={styles.image} resizeMode={FastImage.resizeMode.cover} />
         </View>
       </TouchableOpacity>
-      <Modal visible={isImageViewVisible} onRequestClose={() => setImageViewVisible(false)}>
+      <Modal animationType="fade" transparent={false} visible={isImageViewVisible} onRequestClose={() => setImageViewVisible(false)}>
         <View style={{ flex: 1, backgroundColor: BLACK }}>
-          <PhotoView
-            source={require('../../../../resources/image/map.png')}
-            minimumZoomScale={1}
-            maximumZoomScale={3}
-            androidScaleType="center"
-            onLoad={() => console.log('Image loaded!')}
-            style={{ height: '100%', flex: 1 }}
-          />
+          <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+            <View style={{ padding: 15, backgroundColor: BLACK }}>
+              <Button title="X" color={BLACK} onPress={() => setImageViewVisible(false)} />
+            </View>
+          </View>
+          <View style={{ flex: 1, overflow: 'hidden' }}>
+            <PhotoView
+              source={require('../../../../resources/image/map.png')}
+              minimumZoomScale={1}
+              maximumZoomScale={3}
+              androidScaleType="center"
+              onLoad={() => console.log('Image loaded!')}
+              style={{ height: '100%', flex: 1 }}
+            />
+          </View>
+          <View><TouchableOpacity><Text style={{ color: WHITE }}>Карта торгового центра</Text></TouchableOpacity></View>
         </View>
       </Modal>
     </BlockTitleAndButton>
