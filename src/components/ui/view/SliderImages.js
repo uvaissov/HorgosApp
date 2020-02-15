1c8a8690086922120bcddf1382e68d613b929862
Serik Uvaissov <uvaissov@gmail.com>
2020-02-15 20:21:29 +0600

    slider images cache and 25 build

--- src/components/ui/view/SliderImages.js
+++ src/components/ui/view/SliderImages.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
-import { View, StyleSheet, TouchableWithoutFeedback, TouchableOpacity, Modal, ScrollView } from 'react-native'
+import { View, StyleSheet, TouchableWithoutFeedback, TouchableOpacity, Modal } from 'react-native'
 import FastImage from 'react-native-fast-image'
 import nextId from 'react-id-generator'
 import Swiper from 'react-native-swiper'
 import ReactNativeZoomableView from '@dudigital/react-native-zoomable-view/src/ReactNativeZoomableView'
 import Feather from 'react-native-vector-icons/Feather'
 import _ from 'lodash'
-import { GRAY_SECOND, h, w, WHITE } from '../../../constants/global'
+import { GRAY_SECOND, statusBarHeight, w, WHITE } from '../../../constants/global'
 
 const width = w - 30
 const height = width * 0.5
@@ -28,8 +28,8 @@ const _renderSwiper = (data) => {
   //const images = data.map((el) => ({ source: el }))
   const [isImageViewVisible, setImageViewVisible] = useState(false)
   const [selected, setSelected] = useState(0)
-  const [scrollEnabled] = useState(true)
-  //const imageView = useRef(null)
+  //const [scrollEnabled, setScrollEnabled] = useState(true)
+  //const scroll = useRef(null)
   return (
     <View>
       <Swiper index={selected} paginationStyle={{ marginHorizontal: 15 }} key={nextId()} height={height} activeDotColor={WHITE} dotStyle={{ borderColor: 'rgba(255,255,255,0.8)', borderWidth: 1 }}>
@@ -55,36 +55,22 @@ const _renderSwiper = (data) => {
       </Swiper>
       <Modal visible={isImageViewVisible} onRequestClose={() => setImageViewVisible(false)}>
         <View style={styles.modalBody} onStartShouldSetResponder={() => true}>
-          <View style={{ zIndex: 1500, position: 'absolute', right: 15, top: 15, padding: 10, borderRadius: 50, backgroundColor: GRAY_SECOND, borderWidth: 1, borderColor: GRAY_SECOND }}>
+          <View style={{ zIndex: 1500, position: 'absolute', right: 15, top: statusBarHeight + 15, padding: 10, borderRadius: 50, backgroundColor: GRAY_SECOND, borderWidth: 1, borderColor: GRAY_SECOND }}>
             <TouchableOpacity onPress={() => setImageViewVisible(false)} hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }}>
               <Feather name="x" size={20} color={WHITE} />
             </TouchableOpacity>
           </View>
-          <ScrollView scrollEnabled={scrollEnabled} horizontal pagingEnabled style={[styles.body]}>
-            {
-                data.map((item) => (
-                  <View key={nextId()} style={[{ width: w, height: h }]}>
-                    <ReactNativeZoomableView
-                      // onZoomBefore={() => {
-                      //   setScrollEnabled(false)
-                      // }}
-                      // onZoomEnd={() => {
-                      //   setScrollEnabled(true)
-                      // }}
-                      maxZoom={3}
-                      minZoom={0.5}
-                      zoomStep={0.5}
-                      initialZoom={1}
-                      bindToBorders
-                      zoomEnabled
-                      captureEvent
-                    >
-                      <FastImage source={item} style={styles.body} resizeMode={FastImage.resizeMode.contain} />
-                    </ReactNativeZoomableView>
-                  </View>
-                ))
-            }
-          </ScrollView>
+          <ReactNativeZoomableView
+            maxZoom={3}
+            minZoom={0.5}
+            zoomStep={0.5}
+            initialZoom={1}
+            bindToBorders
+            zoomEnabled
+            captureEvent
+          >
+            <FastImage source={data[selected]} style={styles.body} resizeMode={FastImage.resizeMode.contain} />
+          </ReactNativeZoomableView>
         </View>
       </Modal>
     </View>
